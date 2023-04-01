ace898e94989a47593c1f67466869cb11a8f9f8e
VALNEY MAIA <93133793+valneymaia@users.noreply.github.com>
2023-04-01 10:23:27 -0300

    Update Campeonato.java

--- Campeonato.java
+++ Campeonato.java
@@ -31,7 +31,7 @@ public class Campeonato {
         int golsM= rand.nextInt(6);
         int golsV= rand.nextInt(6);
         //printa o resultados das partidas dos mandantes e visitante
-        //System.out.println(clubeMandante.nomeTime + " " + golsM + " X " + golsV + " " + clubeVisitante.nomeTime );
+        System.out.println(clubeMandante.nomeTime + " " + golsM + " X " + golsV + " " + clubeVisitante.nomeTime );
         //checar os gols e salva o resultado da partida
         if(golsM>golsV){
             clubeMandante.ganharJogo(golsM, golsV);
@@ -44,20 +44,7 @@ public class Campeonato {
             clubeVisitante.empatarJogo(golsV, golsM);
         }
     }
-    //implemtaçao para printar a classicaçao usando metodo void
-    /*public void getClassificacao() {
-        Collections.sort(clubes, Clube.comparePontos);
-       // for(Clube clube : clubes) {  //teste de ordenaçao quando empatava nos pontos
-        //    System.out.println(clube.getNomeTime() + "Pontos: " + clube.getPontos() + ", saldo de gols: " + clube.getSaldodeGols());
-       // }
-        Collections.sort(clubes, Clube.compareSaldo);
-        for(Clube clube : clubes) {
-            System.out.println("---------------------------------------------------------");
-            System.out.println("|"+clube.getNomeTime() + "\t  |Pontos: |" + clube.getPontos() + "|\t saldo de gols: |" + clube.getSaldodeGols()+"|");
-        } 
-        System.out.println("---------------------------------------------------------");
-    }*/
-    // printar a  classificacao como string 
+   
     public String getClassificacao() {
         Collections.sort(clubes, Clube.comparePontos);
        // for(Clube clube : clubes) {  //teste de ordenaçao quando empatava nos pontos
@@ -72,27 +59,29 @@ public class Campeonato {
         } 
     return sb.toString();
     }
-    //printa o campeao, quem vai para libertadores, quem vai para sul americana e os rebaixados 
-    public void getCampeao(){ 
-        System.out.println("\n\tPARABENS CAMPEAO: "+ clubes.get(0).getNomeTime());
+
+   public String getCampeao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\tPARABENS CAMPEAO: "+ clubes.get(0).getNomeTime());
+        sb.append("\n============================================================");
         int n = clubes.size();
         for(n =0; n < 4 ; n++){
-            System.out.println("clasificados para libertadores: "+ clubes.get(n).getNomeTime());
+            sb.append("\nclasificados para libertadores: "+ clubes.get(n).getNomeTime());
         }
-        System.out.println("---------------------------------------------------------");
-        for(n =4; n < 6 ; n++){
-            System.out.println("clasificados para pre-libertadores: "+ clubes.get(n).getNomeTime());
+        sb.append("\n============================================================");
+        for(n = 4; n < 6 ; n++){
+            sb.append("\nclasificados para pre-libertadores: "+ clubes.get(n).getNomeTime());
         }
-        System.out.println("---------------------------------------------------------");
+        sb.append("\n============================================================");
         for(n =6; n < 12 ; n++){
-            System.out.println("clasificados para sula-americana: "+ clubes.get(n).getNomeTime());
+            sb.append("\nclasificados para sula-americana: "+ clubes.get(n).getNomeTime());
         }
-        System.out.println("---------------------------------------------------------");
+        sb.append("\n============================================================");
         for(n =16; n < 20; n++){
-            System.out.println("rebaixado para serie B: "+ clubes.get(n).getNomeTime());
+            sb.append("\nrebaixado para serie B: "+ clubes.get(n).getNomeTime());
         }
-        System.out.println("---------------------------------------------------------\n");
-    }
+        return sb.toString();
+   }
 }
 
    
